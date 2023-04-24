4042b9e99bd618049aa618d2750b7428d7a2f0f3
Kevin Curruchich <kevinc@mayfer.dev>
2023-04-23 22:46:02 -0600

    fix: delete migration history

--- prisma/migrations/20230419062019_option_student_types/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "StudentType" DROP CONSTRAINT "StudentType_studentTypeStudent_fkey";
-
--- AlterTable
-ALTER TABLE "StudentType" ALTER COLUMN "studentTypeStudent" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "StudentType" ADD CONSTRAINT "StudentType_studentTypeStudent_fkey" FOREIGN KEY ("studentTypeStudent") REFERENCES "Student"("studentId") ON DELETE SET NULL ON UPDATE CASCADE;
